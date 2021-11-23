f7b54e9270eb258a91df2c748d018f54fda893c5
k.sliz <kinga.pawliszyn@gmail.com>
2021-11-24 00:19:25 +0100

    before drag and drop

--- src/app/components/dressup-options/dressup-options.component.ts
+++ src/app/components/dressup-options/dressup-options.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
-type Option = 'top' | 'bottom' | 'shoes';
+type Option = 'top' | 'bottom' | 'shoes' | 'hair' | 'accessories';
 @Component({
   selector: 'app-dressup-options',
   templateUrl: './dressup-options.component.html',
   styleUrls: ['./dressup-options.component.scss'],
 })
 export class DressupOptionsComponent implements OnInit {
-  readonly options: Option[] = ['top', 'bottom', 'shoes'];
+  readonly options: Option[] = [
+    'top',
+    'bottom',
+    'shoes',
+    'hair',
+    'accessories',
+  ];
 
   chosenOption: Option = 'top';
   constructor() {}
