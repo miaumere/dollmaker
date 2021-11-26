24c6e470a306b96c00c3ffa2e6dbcae6dd8178d8
k.sliz <kinga.pawliszyn@gmail.com>
2021-11-26 13:39:44 +0100

    rr

--- src/app/components/dressup-options/dressup-options.component.ts
+++ src/app/components/dressup-options/dressup-options.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  fromEvent,
+  switchMap,
+  map,
+  takeUntil,
+  last,
+  combineLatest,
+  tap,
+} from 'rxjs';
 
 type Option = 'top' | 'bottom' | 'shoes' | 'hair' | 'accessories';
 @Component({
@@ -16,59 +25,111 @@ export class DressupOptionsComponent implements OnInit {
   ];
 
   chosenOption: Option = 'top';
+  isDown: boolean = false;
+  offset = [0, 0];
+
+  mouseMove$ = fromEvent(document, 'mousemove');
+  mouseUp$ = fromEvent(document, 'mouseup');
+
+  draggableElements = document.getElementsByClassName('draggable');
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    Array.from(this.draggableElements).forEach((x) =>
+      this.createDraggableElement(x)
+    );
+
+    Array.from(this.draggableElements).forEach((element: any, i: any) => {
+      element.addEventListener('mydragstart', () =>
+        console.log(`mydragstart on element #${i}`)
+      );
+
+      element.addEventListener('mydragmove', (event: any) =>
+        console.log(
+          `mydragmove on element #${i}`,
+          `delta: (${event.detail.deltaX}, ${event.detail.deltaY})`
+        )
+      );
+
+      element.addEventListener('mydragend', (event: any) =>
+        console.log(
+          `mydragend on element #${i}`,
+          `delta: (${event.detail.deltaX}, ${event.detail.deltaY})`
+        )
+      );
+    });
+  }
 
   setChosenOption(option: Option): void {
     this.chosenOption = option;
   }
 
-  handleDragstart(event: any) {
-    // FIXME
-    console.log('drag starts');
-    event.dataTransfer.setData('text/plain', event.target.id);
-    event.dataTransfer.dropEffect = 'copy';
+  //
 
-    // setTimeout(() => {
-    //   event.target.classList.add('hide');
-    // }, 0);
-  }
+  createDraggableElement(element: any) {
+    const mouseDown$ = fromEvent(element, 'mousedown');
 
-  moveAt(pageX: number, pageY: number, item: any) {
-    // FIXME
-    item.style.left = pageX - item.offsetWidth / 2 + 'px';
-    item.style.top = pageY - item.offsetHeight / 2 + 'px';
-  }
+    const dragStart$ = mouseDown$;
+    const dragMove$ = dragStart$.pipe(
+      switchMap((start: any) =>
+        this.mouseMove$.pipe(
+          map((moveEvent: any) => ({
+            originalEvent: moveEvent,
+            deltaX: moveEvent.pageX - start.pageX,
+            deltaY: moveEvent.pageY - start.pageY,
+            startOffsetX: start.offsetX,
+            startOffsetY: start.offsetY,
+          })),
+          takeUntil(this.mouseUp$)
+        )
+      )
+    );
+    const dragEnd$ = dragStart$.pipe(
+      switchMap((start: any) =>
+        this.mouseMove$.pipe(
+          map((moveEvent: any) => ({
+            originalEvent: moveEvent,
+            deltaX: moveEvent.pageX - start.pageX,
+            deltaY: moveEvent.pageY - start.pageY,
+            startOffsetX: start.offsetX,
+            startOffsetY: start.offsetY,
+          })),
+          takeUntil(this.mouseUp$),
+          last()
+        )
+      )
+    );
 
-  dragEnter(event: any) {
-    // FIXME
-    event.preventDefault();
-    event.target.classList.add('drag-over');
-  }
+    combineLatest([
+      dragStart$.pipe(
+        tap((event) => {
+          element.dispatchEvent(
+            new CustomEvent('mydragstart', { detail: event })
+          );
+        })
+      ),
+      dragMove$.pipe(
+        tap((event) => {
+          element.dispatchEvent(
+            new CustomEvent('mydragmove', { detail: event })
+          );
+        })
+      ),
+      dragEnd$.pipe(
+        tap((event) => {
+          element.dispatchEvent(
+            new CustomEvent('mydragend', { detail: event })
+          );
+        })
+      ),
+    ]).subscribe();
 
-  dragOver(event: any) {
-    // FIXME
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
-
-    event.target.classList.add('drag-over');
-  }
-
-  dragLeave(event: any) {
-    // FIXME
-    event.target.classList.remove('drag-over');
-  }
-
-  drop(event: any) {
-    event.target.classList.remove('drag-over');
-    const id = event.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id);
-    event.target.appendChild(draggable);
-
-    // let shiftX = event.clientX - ball.getBoundingClientRect().left;
-    // let shiftY = event.clientY - ball.getBoundingClientRect().top;
-
-    // draggable?.classList.remove('hide');
+    dragMove$.subscribe((move: any) => {
+      const offsetX = move.originalEvent.x - move.startOffsetX;
+      const offsetY = move.originalEvent.y - move.startOffsetY;
+      element.style.left = offsetX + 'px';
+      element.style.top = offsetY + 'px';
+    });
   }
 }
